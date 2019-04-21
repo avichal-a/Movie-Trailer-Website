e4e974ff672045d7979aeffefa1093b8e683af94
Avichal <avichalagarwal@hotmail.com>
2019-04-21 11:34:58 +0530

    sumitting after review
    Signed-off-by: Avichal <avichalagarwal@hotmail.com>

--- media.py
+++ media.py
@@ -1,9 +1,24 @@
 import webbrowser
+
+
 class Movie():
-	def __init__(M,title,story,poster,traier):
-		M.title = title
-		M.m_story = story
-		M.poster_image_url =poster
-		M.trailer_youtube_url = traier
-	def play_trailer(M):
-		webbrowser.open(M.trailer_youtube_url)
+
+    """ Defining Class Movie
+    Data members : Movie Title , Stoty line
+    Poster image URL, Youtube triller URL
+    """
+
+    def __init__(M, title, story, poster, traier):
+    	    """ Constructor of class Movie
+            Takes arguments: Movie Title , Stoty line
+            Poster image URL, Youtube triller URL
+            """
+            M.title = title
+            M.m_story = story
+            M.poster_image_url = poster
+            M.trailer_youtube_url = traier
+
+    def play_trailer(M):
+        """ OPens the yoube trailer URL in browser
+        """
+        webbrowser.open(M.trailer_youtube_url)
